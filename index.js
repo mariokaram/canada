6541f0eaf9d6c68a8626f0a18f8cb896a77ba0be
Mario Karam <m.karam@france.acteos.com>
2021-12-10 15:40:13 +0200

    fix remaining days

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import fetch from "node-fetch";
 import express from "express";
+import moment from "moment";
 
 const app = express();
 
@@ -17,7 +18,27 @@ app.use("/", async (req, res, next) => {
     }
   };
 
-  res.render("index.pug", { data: await fetchTime() });
+  const dayINeed = 2; // for tuesday
+  const today = moment().isoWeekday();
+  let date;
+
+  // if we haven't yet passed the day of the week that I need:
+  if (today < dayINeed) {
+    // then just give me this week's instance of that day
+    date = moment().isoWeekday(dayINeed).format("DD/MM/YYYY");
+  } else {
+    // otherwise, give me next week's instance of that same day
+    date = moment().add(1, "weeks").isoWeekday(dayINeed).format("DD/MM/YYYY");
+  }
+  let remainingDays = Math.ceil(
+    moment(date, "DD/MM/YYYY").diff(moment(), "days", true)
+  );
+
+  res.render("index.pug", {
+    data: await fetchTime(),
+    update: date,
+    remainingDays,
+  });
 });
 
 app.listen(process.env.PORT || 8000, () => {
